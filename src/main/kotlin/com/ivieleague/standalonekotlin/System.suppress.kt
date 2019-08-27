d7bacb9601d314c1b1e0c9408d77a89ddb069cdd
Joseph Ivie <josephivie@gmail.com>
2019-08-26 19:02:51 -0600

    Option to create file when edit flag is used

--- src/main/kotlin/com/ivieleague/standalonekotlin/System.suppress.kt
+++ src/main/kotlin/com/ivieleague/standalonekotlin/System.suppress.kt
@@ -3,9 +3,26 @@ package com.ivieleague.standalonekotlin
 import java.io.OutputStream
 import java.io.PrintStream
 
+class NullOutputStream() : OutputStream() {
+    override fun write(b: Int) {
+    }
+
+    override fun write(b: ByteArray) {
+    }
+
+    override fun write(b: ByteArray, off: Int, len: Int) {
+    }
+
+    override fun flush() {
+    }
+
+    override fun close() {
+    }
+}
+
 inline fun <T> suppressStandardError(action: () -> T): T {
     val realOut = System.err
-    System.setErr(PrintStream(OutputStream.nullOutputStream()))
+    System.setErr(PrintStream(NullOutputStream()))
     val result = action()
     System.setErr(realOut)
     return result
@@ -13,7 +30,7 @@ inline fun <T> suppressStandardError(action: () -> T): T {
 
 inline fun <T> suppressStandardOutput(action: () -> T): T {
     val realOut = System.out
-    System.setOut(PrintStream(OutputStream.nullOutputStream()))
+    System.setOut(PrintStream(NullOutputStream()))
     val result = action()
     System.setOut(realOut)
     return result
