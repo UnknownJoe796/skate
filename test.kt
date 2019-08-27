235b131f75be750d98a2ed12fe803a2c8c6e087e
Joseph Ivie <josephivie@gmail.com>
2019-08-26 19:45:12 -0600

    Opens file immediately on opening the project

--- test.kt
+++ test.kt
@@ -1,10 +1,13 @@
 @file:Repository("https://dl.bintray.com/lightningkite/com.lightningkite.kotlin")
-@file:DependsOn("com.lightningkite.kotlin:okhttp-jackson:0.6.3")
+@file:DependsOn("com.lightningkite.kotlin:okhttp-jackson:0.6.5")
 
 package com.test
 
+import lk.kotlin.jvm.utils.async.Async
+import lk.kotlin.okhttp.defaultClient
 import lk.kotlin.okhttp.jackson.lambdaJackson
 import okhttp3.Request
+import okhttp3.Response
 
 val hello = "Hello World!"
 
@@ -25,6 +28,9 @@ fun main() {
         .result?.let {
         it.forEach { println(it) }
     }
-}
 
+    defaultClient.dispatcher().executorService().shutdown()
+    Async.shutdown()
+    println("Shut down")
+}
 //Test comment 2
